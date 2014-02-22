b73eedbd572fdeeb17266a1443947b4ca81887cd
idolife <csb4game@163.com>
2014-02-22 13:43:20 +0800

    Add missing files

---
+++ Test/NSF.Test.RPC.Perf.Svc/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using NSF.Share;
+using NSF.Framework.Net.RPC;
+using NSF.Test.RPC.Protocol;
+
+namespace NSF.Test.RPC.Perf.Svc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RpcEchoProtocol impl = new RpcEchoProtocol();
+            RpcServer svc = new RpcServer();
+            svc.Init(new IPEndPoint(IPAddress.Any, 7000), impl);
+            Console.ReadKey();
+        }
+    }
+}
